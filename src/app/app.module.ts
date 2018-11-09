e61561651caec0117e382700c79f59fe26a88df2
Yan Zhang <zhangyan612@gmail.com>
2018-11-08 23:01:25 -0600

    Add test reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 import { RouterModule } from '@angular/router'
 
@@ -27,6 +27,7 @@ import { QuoteComponent } from './quote/quote.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     NgbModule,
     RouterModule.forRoot([
