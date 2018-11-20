c5b38e501d86037f3604aa82000442314ebc2fca
Yan Zhang <zhangyan612@gmail.com>
2018-11-19 19:51:38 -0600

    update data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,8 +8,8 @@ import { catchError, tap, map } from 'rxjs/operators'
   providedIn: 'root'
 })
 export class DataService {
-  //private apiUrl = 'http://localhost:57602/api/'  ;  
-  private apiUrl = 'https://my-json-server.typicode.com/zhangyan612/jsonfile/'
+  private apiUrl = 'http://localhost:57602/api/'  ;  
+  //private apiUrl = 'https://my-json-server.typicode.com/zhangyan612/jsonfile/'
 
   private header = new HttpHeaders({'Content-Type':  'application/json'})
   private httpOptions = {
